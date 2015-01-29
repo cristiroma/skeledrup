b9af2ec2dc930a167d6881e821e352b3d07d29df
Cristian Romanescu <cristian@romanescu.ro>
2015-01-29 19:56:07 +0200

    Select installation profile

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -13,3 +13,34 @@ if (file_exists($config_yml)) {
   $config = yaml_parse(file_get_contents($config_yml));
   $project_config += $config;
 }
+
+// Utility functions
+
+function rrmdir($dir) {
+  if (is_dir($dir)) {
+    $objects = scandir($dir);
+    foreach ($objects as $object) {
+      if ($object != "." && $object != "..") {
+        if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+      }
+    }
+    reset($objects);
+    rmdir($dir);
+  }
+}
+
+function rcopy($src, $dst) {
+  $dir = opendir($src);
+  @mkdir($dst);
+  while(false !== ( $file = readdir($dir)) ) {
+    if (( $file != '.' ) && ( $file != '..' )) {
+      if ( is_dir($src . '/' . $file) ) {
+        rcopy($src . '/' . $file,$dst . '/' . $file);
+      }
+      else {
+        copy($src . '/' . $file,$dst . '/' . $file);
+      }
+    }
+  }
+  closedir($dir);
+}
